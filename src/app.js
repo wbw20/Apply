407829a89252cd038240b5b64d906dd1bb58ae44
William B. Wettersten <wbw20@case.edu>
2013-08-04 09:10:29 -0700

    hello world

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
 var express = require('express');
 
+var app = express();
+
+app.get('/agent', function(req, res) {
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+app.listen(8080);
