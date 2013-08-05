eedcdb5abcefcdd83e7ceab04730f547ac669f69
William B. Wettersten <wbw20@case.edu>
2013-08-04 19:59:36 -0700

    assets served correctly

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ var ejs = require('ejs');
 var app = express();
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
+app.use(express.static(__dirname + '/static'));
 app.set('views', __dirname + '/static');
 
 app.get('/agent', function(req, res) {
   res.render('agent');
-  console.log(express.static());
 });
 
 app.listen(8080);
