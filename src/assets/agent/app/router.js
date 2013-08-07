608fc296ead6971763cb91a884db167e291971fb
William B. Wettersten <wbw20@case.edu>
2013-08-06 22:59:45 -0700

    looks like we need to build html into one file

--- src/assets/agent/app/router.js
+++ src/assets/agent/app/router.js
@@ -2,9 +2,3 @@ App.Router.map(function () {
   this.resource('dashboard', { path: '/' });
   this.resource('menu', { path: '/menu' });
 });
-
-App.MenuRoute = Ember.Route.extend({
-  renderTemplate: function(controller) {
-    this.render('views/dashboard/menu', {controller: controller});
-  }
-});
