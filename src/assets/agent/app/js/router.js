f9dd67905432bdcfce6737e3a0a5eb00e112d29f
William B. Wettersten <wbw20@case.edu>
2013-08-08 23:02:51 -0700

    link kind of works

--- src/assets/agent/app/js/router.js
+++ src/assets/agent/app/js/router.js
@@ -1,10 +1,5 @@
 App.Router.map(function () {
-  this.resource('index', { path: '/' });
-  this.resource('workspace', { path: '/workspace' });
-});
-
-App.WorkspaceRoute = Ember.Route.extend({
-  renderTemplate: function() {
-    this.render({ outlet: 'workspace' });
-  }
+  this.resource('index', { path: '/' }, function() {
+    this.route('workspace');
+  });
 });
