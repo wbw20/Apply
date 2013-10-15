b5522cbfd222b3af59bd88dbd2641ec95d6c5b00
William B. Wettersten <wbw20@case.edu>
2013-10-15 15:34:33 -0400

    custom outlet

--- src/assets/agent/app/js/router.js
+++ src/assets/agent/app/js/router.js
@@ -2,3 +2,17 @@ App.Router.map(function() {
   this.resource('workspace');
   this.resource('profile');
 });
+
+App.WorkspaceRoute = Ember.Route.extend({
+  model: function() {
+    return {
+      title: "yayyyy"
+    };
+  },
+  renderTemplate: function() {
+    this._super();
+    this.render("workspace", {
+      outlet: 'dashboard'
+    });
+  }
+});
