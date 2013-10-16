afe288da048ca620e5374cc563203af966992e64
William B. Wettersten <wbw20@case.edu>
2013-10-15 23:13:13 -0400

    some dummy applications

--- src/assets/agent/app/js/router.js
+++ src/assets/agent/app/js/router.js
@@ -2,3 +2,9 @@ App.Router.map(function() {
   this.resource('workspace');
   this.resource('profile');
 });
+
+App.WorkspaceRoute = Ember.Route.extend({
+  model: function(params) {
+    return ['an app', 'another app'];
+  }
+});
