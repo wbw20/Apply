ef277d97ec2d0989f0c7527ff9cea1bc0501c196
William B. Wettersten <wbw20@case.edu>
2013-10-17 20:03:34 -0400

    tab addition POC

--- src/assets/agent/app/js/views/header_view.js
+++ src/assets/agent/app/js/views/header_view.js
@@ -1,3 +1,10 @@
 App.HeaderView = Ember.View.extend({
-  templateName: 'header'
+  templateName: 'header',
+  click: function(event) {
+    if ($(event.target)[0].id == 'newtab') {
+      $('#tabroot').append('<li class="active">' +
+                             '<a href="#">Home</a>' +
+                           '</li>');
+    }
+  }
 });
