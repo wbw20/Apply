49ea8451f401e16934e9336dd466b313cc1a7b45
William B. Wettersten <wbw20@case.edu>
2013-10-20 18:05:21 -0400

    append all new tabs to be the second tab

--- src/assets/agent/app/js/controller/application_controller.js
+++ src/assets/agent/app/js/controller/application_controller.js
@@ -3,7 +3,7 @@ var open = [];
 App.ApplicationController = Ember.Controller.extend({
   newtab: function(model) {
     if (!open.contains(model.id)) {
-      $('#tabroot').append('<li class="active">' +
+      $('#firsttab').after('<li class="active">' +
                              '<a href="#/submissions/' + model.id + '">' + model.name + '</a>' +
                            '</li>');
       open.push(model.id);
