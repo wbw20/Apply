61c5bf06ce911398d4ddc9e2742b38fde92d318b
William B. Wettersten <wbw20@case.edu>
2013-10-19 22:53:09 -0400

    new ids and a new route

--- src/assets/agent/app/js/router.js
+++ src/assets/agent/app/js/router.js
@@ -1,19 +1,24 @@
 App.Router.map(function() {
-  this.resource('workspace');
+  this.resource('workspace', function() {
+    this.route("submission", { path: "/:submission_id" });
+  });
   this.resource('profile');
 });
 
 App.WorkspaceRoute = Ember.Route.extend({
   model: function(params) {
     return [{
+        id: 0,
         name: "Will",
         major: "Computer Science",
         gpa: "3.0"
     }, {
+        id: 1,
         name: "Charles",
         major: "Barking",
         gpa: "4.0"
     }, {
+        id: 2,
         name: "Ginny",
         major: "Psychology premed",
         gpa: "3.5"
