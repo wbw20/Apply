c60d21ddc5beadd19f83c020510347fa9390f2b8
William B. Wettersten <wbw20@case.edu>
2013-10-22 14:01:15 -0400

    implement POST for submissions

--- src/controllers/submission_controller.js
+++ src/controllers/submission_controller.js
@@ -4,16 +4,24 @@ module.exports = {
   setup: function(app) {
     app.get('/submission/:id?', function(req, res) {
       if (req.id) {
-        res.send(req.id);
+        Submission.find(req.id, function(error, data) {
+          res.send(data);
+        });
       } else {
-        Submission.all(function(err, data) {
+        Submission.all(function(error, data) {
           res.send(data);
         });
       }
     });
 
     app.post('/submission', function(req, res) {
-      //TODO
+      Submission.create(function(error) {
+        if (error) {
+          res.send(500, error);
+        } else {
+          res.send(200);
+        }
+      })
     });
   }
 };
--- src/models/submission.js
+++ src/models/submission.js
@@ -1,7 +1,7 @@
 var dao = require('../dao');
 
 var Submission = dao.define('submission', {
-    submitted: Date
+    submitted: { type: Date, default: Date.now }
 });
 
 dao.autoupdate(function() {
