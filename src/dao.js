843b448a8a89cadb1fb655afb996892457582307
William B. Wettersten <wbw20@case.edu>
2013-10-21 20:52:41 -0400

    properly sync new orm models

--- src/dao.js
+++ src/dao.js
@@ -5,10 +5,17 @@ var credentials = JSON.parse(fs.readFileSync('conf/properties.json').toString())
 var url = "mysql://" + credentials.db.username + ":" + credentials.db.password +
             "@" + credentials.db.host + "/" + credentials.db.database;
 
+var db_connect = function(callback) {
+  orm.connect(url, function(error, connection) {
+    callback(connection);
+  });
+}
+
+db_connect(require('./models/submission'));
+db_connect(function(connection) {
+  connection.sync();
+});
+
 module.exports = Object.freeze({
-  connect: function(callback) {
-    orm.connect(url, function(error, connection) {
-      callback(connection);
-    });
-  }
+  connect: db_connect
 });
