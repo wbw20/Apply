777a5b8fc924f73e2137559c2ddbdde574e588e2
William B. Wettersten <wbw20@case.edu>
2013-10-25 16:19:59 -0400

    applicant controller

--- src/controllers/applicant_controller.js
+++ src/controllers/applicant_controller.js
@@ -2,6 +2,18 @@ var Applicant = require('../models/applicant').Applicant;
 
 module.exports = {
   setup: function(app) {
+    app.get('/applicant/:id?', function(req, res) {
+      if (req.id) {
+        Applicant.find(req.id, function(error, data) {
+          res.send(data);
+        });
+      } else {
+        Applicant.all(function(error, data) {
+          res.send(data);
+        });
+      }
+    });
+
     app.post('/applicant', function(req, res) {
       Applicant.create(req.body, function(error) {
         if (error) {
@@ -9,7 +21,11 @@ module.exports = {
         } else {
           res.send(200);
         }
-      });
+      })
+    });
+
+    app.put('/applicant', function(req, res) {
+      //TODO
     });
   }
 };
