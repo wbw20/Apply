575e959f46aeedf2a07b61a84e5ccf17062c801e
William B. Wettersten <wbw20@case.edu>
2013-10-25 16:00:17 -0400

    okay, we cant force inheritance

--- src/models/user.js
+++ src/models/user.js
@@ -2,25 +2,15 @@ var dao = require('../dao');
 var Submission = require('./submission');
 require('../lib/extend');
 
-var User = Class.extend(dao.define('user', {
+var Agent = Class.extend(dao.define('agent', {
   first: { type: String, length: 32 },
-  last:  { type: String, length: 32 },
-  role:  { type: String, length: 32 }
+  last:  { type: String, length: 32 }
 }));
 
-var Agent = User.extend({
-  create: function(properties) {
-    properties.role = 'agent';
-    this._super(properties);
-  }
-});
-
-var Applicant = User.extend({
-  create: function(properties) {
-    properties.role = 'applicant';
-    this._super(properties);
-  }
-});
+var Applicant = Class.extend(dao.define('agent', {
+  first: { type: String, length: 32 },
+  last:  { type: String, length: 32 }
+}));
 
 module.exports = {
   Agent: Agent,
