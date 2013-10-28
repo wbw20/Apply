972e63c9ded4d4ec8a1af34e64bc4a7be4a0c4af
William B. Wettersten <wbw20@case.edu>
2013-10-27 21:40:16 -0400

    display applicant name in tab correctly

--- src/assets/agent/app/js/controller/application_controller.js
+++ src/assets/agent/app/js/controller/application_controller.js
@@ -4,7 +4,8 @@ App.ApplicationController = Ember.Controller.extend({
   newtab: function(model) {
     if (!open.contains(model.id)) {
       $('#firsttab').after('<li class="active">' +
-                             '<a href="#/submissions/' + model.id + '">' + model.name + '</a>' +
+                             '<a href="#/submissions/' + model.id + '">' + model.applicant.first +
+                             ' ' + model.applicant.last + '</a>' +
                            '</li>');
       open.push(model.id);
     }
