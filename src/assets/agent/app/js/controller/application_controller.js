760f62405953b0beb53de5ba1c4b28e6804b298d
William B. Wettersten <wbw20@case.edu>
2013-10-28 09:18:08 -0400

    [].contains is undefined I guess

--- src/assets/agent/app/js/controller/application_controller.js
+++ src/assets/agent/app/js/controller/application_controller.js
@@ -2,7 +2,7 @@ var open = [];
 
 App.ApplicationController = Ember.Controller.extend({
   newtab: function(model) {
-    if (!open.contains(model.id)) {
+    if (open.indexOf(model.id) == -1) {
       $('#firsttab').after('<li class="active">' +
                              '<a href="#/submissions/' + model.id + '">' + model.applicant.first +
                              ' ' + model.applicant.last + '</a>' +
