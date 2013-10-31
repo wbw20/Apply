c7c35e6b79521dc245d7042caba37efbd34b74a7
William B. Wettersten <wbw20@case.edu>
2013-10-31 09:21:27 -0400

    prevent duplicant submission tabs

--- src/assets/agent/app/js/controller/application_controller.js
+++ src/assets/agent/app/js/controller/application_controller.js
@@ -1,3 +1,5 @@
+var tabs = [];
+
 App.ApplicationController = Ember.ArrayController.extend({
   init: function() {
     this.set('content', [{
@@ -10,9 +12,20 @@ App.ApplicationController = Ember.ArrayController.extend({
   },
   open: function(tab) {
     if (!this.contains(tab)) {
+      tabs.push(tab);
       this.pushObject(tab);
     }
 
     this.transitionToRoute(tab.route, tab.model);
+  },
+  contains: function(newtab) {
+    var content = this.get('content');
+    for (i=0; i < content.length; i++) {
+      if (newtab.route === content[i].route && newtab.model.id == content[i].model.id) {
+        return true;
+      }
+    }
+
+    return false;
   }
 });
