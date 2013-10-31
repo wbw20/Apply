83d137962a1fe1be4b419d5e36b2c3d8aa8f19ea
William B. Wettersten <wbw20@case.edu>
2013-10-31 10:00:41 -0400

    use proper applicant name for tab labels

--- src/assets/agent/app/js/controller/workspace_controller.js
+++ src/assets/agent/app/js/controller/workspace_controller.js
@@ -2,7 +2,7 @@ App.WorkspaceController = Ember.Controller.extend({
   needs: ['application'],
   open: function(model) {
     this.get('controllers.application').open({
-      name: model.first,
+      name: model.applicant.first + model.applicant.last,
       route: 'submission',
       model: model
     });
