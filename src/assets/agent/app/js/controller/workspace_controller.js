753bc59a936c64d997d70e2cc028798ec111cd70
William B. Wettersten <wbw20@case.edu>
2013-10-31 10:02:26 -0400

    use a space between first and last name in tab labels

--- src/assets/agent/app/js/controller/workspace_controller.js
+++ src/assets/agent/app/js/controller/workspace_controller.js
@@ -2,7 +2,7 @@ App.WorkspaceController = Ember.Controller.extend({
   needs: ['application'],
   open: function(model) {
     this.get('controllers.application').open({
-      name: model.applicant.first + model.applicant.last,
+      name: model.applicant.first + ' ' + model.applicant.last,
       route: 'submission',
       model: model
     });
