6d67a453926fbfe24e2e54c95e20b42bf985527d
William B. Wettersten <wbw20@case.edu>
2013-11-04 19:24:38 -0500

    Grunt exporting to a global

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.registerMultiTask('browser', "Export a module to the window", function() {
+    var opts = this.options();
+    this.files.forEach(function(f) {
+      var output = ["(function(globals) {"];
+   
+      output.push.apply(output, f.src.map(grunt.file.read));
+   
+      output.push(grunt.template.process(
+        'window.<%= namespace %> = requireModule("<%= barename %>");', { 
+        data: {
+          namespace: opts.namespace,
+          barename: opts.barename
+        }
+      }));
+      output.push('})(window);');
+   
+      grunt.file.write(f.dest, grunt.template.process(output.join("\n")));
+    });
+  });
+
   grunt.initConfig({
     // concat js and html in assets folder
     concat: {
@@ -23,10 +44,25 @@ module.exports = function(grunt) {
         ],
         tasks: ['concat']
       }
+    },
+    browser: {
+      dist: {
+        src: [
+          'src/assets/agent/app/js/*.js',
+          'src/assets/agent/app/js/**/*.js',
+          'src/assets/agent/app/js/**/**/*.js',
+          'src/assets/agent/app/js/**/**/**/*.js'
+        ],
+        dest: 'src/assets/built/agent.js',
+        options: {
+          barename: 'apply',
+          namespace: "Apply"
+        }
+      }
     }
     // TODO: run tests
   });
 
-  grunt.registerTask('default', ['concat']);
-  grunt.registerTask('server', ['concat', 'watch']);
+  grunt.registerTask('default', ['browser']);
+  grunt.registerTask('server', ['browser', 'watch']);
 };
--- src/assets/agent/app/js/views/row_view.js
+++ src/assets/agent/app/js/views/row_view.js
@@ -2,10 +2,9 @@ App.RowView = Ember.View.extend({
   mouseEnter: function(event) {
     $('li.focused').removeClass('focused');
     $(event.target).addClass('focused');
-    alert("NIGGERSSS");
   },
 
   mouseLeave: function(event) {
     $(event.target).removeClass('focused');
-  }
+  } 
 });
