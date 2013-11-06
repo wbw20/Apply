34d6b0d556233ec5098674c5bedc0b9c1a3fdf77
William B. Wettersten <wbw20@case.edu>
2013-11-06 08:13:14 -0500

    shove the nasty code into a util file

--- src/controllers/submission_controller.js
+++ src/controllers/submission_controller.js
@@ -1,30 +1,6 @@
 var Submission = require('../models/models').Submission;
 var Comment = require('../models/models').Comment;
-
-function join(data) {
-  var results = [];
-  for (var i=0, l=data.length; i<l; i++) {
-    var result = data[i].toObject();
-
-    if (data[i].__cachedRelations) {
-      for (property in data[i].__cachedRelations) {
-        if(Object.prototype.toString.call(data[i].__cachedRelations[property]) === '[object Array]') {
-          var relations = [];
-          for (var j=0, p=data[i].__cachedRelations[property].length; j<p; j++) {
-            relations.push(data[i].__cachedRelations[property][j].__data);
-          }
-          result[property] = relations;
-        } else {
-          result[property] = data[i].__cachedRelations[property].__data;
-        }
-      }
-    }
-
-    results.push(result);
-  }
-
-  return results;
-}
+var util = require('../util');
 
 module.exports = {
   setup: function(app) {
@@ -46,7 +22,7 @@ module.exports = {
       } else {
         Submission.all({ include: ['applicant'] }, function(error, data) {
           Submission.include(data, 'submission_comments', function(error, results) {
-            res.send(join(data));
+            res.send(util.join(data));
           });
         });
       }
