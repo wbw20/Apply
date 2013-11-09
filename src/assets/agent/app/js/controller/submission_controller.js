6a06d4b53e81f1938a936608725200210d620cd1
William B. Wettersten <wbw20@case.edu>
2013-11-08 22:49:57 -0500

    validation on the view

--- src/assets/agent/app/js/controller/submission_controller.js
+++ src/assets/agent/app/js/controller/submission_controller.js
@@ -10,22 +10,5 @@ App.SubmissionController = Ember.ObjectController.extend({
     comment.save();
     this.get('content').submission_comments.pushObject(comment);
     this.set('body', '');
-  },
-  validate: function(comment) {
-      for (var i in comment) {
-          if(comment[i] === '') {
-              this.errors.push({
-                  input: i,
-                  error: 'did not validate'
-              });
-              return false;
-          }
-      }
-      // if no errors
-      return true;
-  },
-  showErrors: function(){
-      // show errors on correct inputs
-      // if input is not in this.errors remove error if its in the DOM
   }
 });
--- src/assets/agent/app/js/views/dashboard/comment/comments_view.js
+++ src/assets/agent/app/js/views/dashboard/comment/comments_view.js
@@ -2,5 +2,12 @@ import { App } from 'application';
 
 App.CommentsView = Ember.View.extend({
   classNames: ['comments'],
-  templateName: 'comments'
+  templateName: 'comments',
+  submit: function(comment) {
+    if (comment) {
+      this.get('controller').submit(comment);
+    } else {
+      alert('enter some text, asshole');
+    }
+  }
 });
