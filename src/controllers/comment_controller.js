5c36d8d46f2333917bcf1950ca143e3f3c3cc6a7
William B. Wettersten <wbw20@case.edu>
2013-11-12 23:55:25 -0500

    require id in GET /v1/submission/:id/comments

--- src/controllers/comment_controller.js
+++ src/controllers/comment_controller.js
@@ -2,16 +2,10 @@ var Comment = require('../models/models').Comment;
 
 module.exports = {
 	setup: function(app){
-    app.get('/v1/submission/:id?/comments', function(req, res) {
-      if (req.params.id) {
-        Comment.find({ submission: req.params.id }, function(error, data) {
-            res.send(result);
-        });
-      } else {
-        Comment.all({ include: 'applicant' }, function(error, data) {
-          res.send(join(data));
-        });
-      }
+    app.get('/v1/submission/:id/comments', function(req, res) {
+      Comment.find({ submission: req.params.id }, function(error, data) {
+          res.send(result);
+      });
     });
 
     app.post('/v1/submission/:id?/comments', function(req, res) {
