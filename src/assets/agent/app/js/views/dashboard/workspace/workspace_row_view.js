2ffa01d3a54263ace70635fd638971573371190d
Brad Davis <brad.bdavis1@gmail.com>
2013-11-17 13:03:21 -0500

    workspace table

--- src/assets/agent/app/js/views/dashboard/workspace/workspace_row_view.js
+++ src/assets/agent/app/js/views/dashboard/workspace/workspace_row_view.js
@@ -1,6 +1,8 @@
 import { App } from 'application';
 
 App.WorkspaceRowView = App.RowView.extend({
+  tagName: 'tr',
+  classNames: ['aplcnt-list-view'],
   click: function(event) {
     this.get('controller').open(this.get('value'));
   }
