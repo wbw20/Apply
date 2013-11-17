1c40be3a64c2463adfc434ea48adeae19072259c
William B. Wettersten <wbw20@case.edu>
2013-11-17 14:02:14 -0500

    no more tmp on master

--- tmp/index.amd.js
+++
@@ -1,8 +0,0 @@
-define("index", 
-  ["router","views/application_view","views/row_view","views/dashboard/comment/comment_view","views/dashboard/comment/comments_view","views/dashboard/workspace/submission_view","views/dashboard/workspace/workspace_row_view","views/dashboard/workspace/workspace_view","views/sidebar/pill_view","models/applicant","models/submission","controller/application_controller","controller/sidebar_controller","controller/submission_controller","controller/workspace_controller","application","exports"],
-  function(__dependency1__, __dependency2__, __dependency3__, __dependency4__, __dependency5__, __dependency6__, __dependency7__, __dependency8__, __dependency9__, __dependency10__, __dependency11__, __dependency12__, __dependency13__, __dependency14__, __dependency15__, __dependency16__, __exports__) {
-    "use strict";
-
-    var App = __dependency16__.App;
-    __exports__.App = App;
-  });
