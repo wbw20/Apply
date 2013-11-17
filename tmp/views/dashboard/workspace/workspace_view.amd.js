cf4b000c96a053ba04499649a9063e6be74901f0
Brad Davis <brad.bdavis1@gmail.com>
2013-11-17 13:49:58 -0500

    removing tmp files

--- tmp/views/dashboard/workspace/workspace_view.amd.js
+++
@@ -1,9 +0,0 @@
-define("views/dashboard/workspace/workspace_view", 
-  ["application"],
-  function(__dependency1__) {
-    "use strict";
-    var App = __dependency1__.App;
-
-    App.WorkspaceView = Ember.View.extend({
-    });
-  });
