27def2f7a32caea9417123e77390cf8954cd0c69
William B. Wettersten <wbw20@case.edu>
2013-11-21 22:59:05 -0500

    add some auth-related columns to agent

--- src/models/agent.js
+++ src/models/agent.js
@@ -1,8 +1,12 @@
 var dao = require('../dao');
 
 var Agent = dao.define('agent', {
-  first: { type: String, length: 32 },
-  last:  { type: String, length: 32 }
+  first:    { type: String, length: 32 },
+  last:     { type: String, length: 32 },
+  username: { type: String, length: 32 },
+  password: { type: String, length: 32 },
+  salt:     { type: String, length: 32 },
+  email:    { type: String, length: 32 }
 });
 
 module.exports = {
